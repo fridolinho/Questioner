9bc487baa036f85bcb97d5fabaaf5ceab3244df8
fridolinho <fridolinho@gmail.com>
2019-01-10 11:23:34 -0800

    added more tests

--- test/test.js
+++ test/test.js
@@ -4,22 +4,27 @@ const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 const server = require('../index');
 
-describe('/ GET all meetups', () => {
-
-	it('it should GET all the meetups', (done) => {
-
+describe('open the homepage', () => {
+	it('it should open the homepage', (done) => {
 		chai.request(server)
-
-			.get('/api/v1/meetup')
-
-			.end((err, res) => {
-                console.log(res.body);
-				res.should.have.status(200);
-				res.body.should.be.a('object');
-				done();
-			});
-
+		.get('/')
+		.end((err, res) => {
+		res.should.have.status(200);
+		done();
+		});
 	});
-
 });
 
+describe('GET all meetups', () => {
+	it('it should GET all the meetups', (done) => {
+		chai.request(server)
+		.get('/api/v1/meetup')
+
+		.end((err, res) => {
+            console.log(res.body);
+			res.should.have.status(200);
+			res.body.should.be.a('object');
+			done();
+		});
+	});
+})
